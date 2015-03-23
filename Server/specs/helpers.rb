eab693bb99372a6359eb4efd88ca9937d376761e
da99 <i-hate-spam-1234567@mailinator.com>
2015-03-23 08:36:11 -0500

    File re-org.

--- Server/specs/helpers.rb
+++ Server/specs/helpers.rb
@@ -1,7 +1,7 @@
 
 require 'Bacon_Colored'
-require './Server/Customer/specs/helpers'
 require 'pry'
+require './Server/Customer/model'
 
 def days_ago_in_sql days
   Sequel.lit(Okdoki::Model::PG::UTC_NOW_RAW + " - interval '#{days * 24} hours'")
@@ -65,6 +65,38 @@ def make_should o, bes_nots, meth, args
   end
 end
 
+
+module Customer_Test
+
+  PSWD = "this is a pass"
+
+  class << self
+
+    def create
+      sn = Screen_Name_Test.new_screen_name
+      Customer.create screen_name: sn,
+        pass_word: PSWD,
+        confirm_pass_word: "this is a pass",
+        ip: '000.00.000'
+    end
+
+    def find n = 0
+      rec = Customer::TABLE.order_by(:id).limit(1, n).last
+
+      c   = Customer.new(rec)
+      sn  = Screen_Name.new(Screen_Name::TABLE[owner_id: c.id, is_sub: false])
+      {c: c, sn: sn, pw: PSWD}
+    end
+
+    def list num = nil
+      @customers ||= [create, create]
+      return @customers if num.nil?
+      @customers[num]
+    end
+
+  end # === class self
+
+end # === module Customer_Test ===
 
 
 
