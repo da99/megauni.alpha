56184a0a6f0edac5f9d568b5f6024dd5f2e23518
da99 <i-hate-spam-1234567@mailinator.com>
2015-03-29 08:04:38 -0500

    Added: styling

--- Server/Root/middleware.rb
+++ Server/Root/middleware.rb
@@ -38,6 +38,8 @@ mu(:FILE_INDEX) {
     link.href('/css/fonts.css')./
     link.href('/css/otfpoc.css')./
 
+    heading_color = '#357BB5'
+    light_text_color = '#8E8E8E'
 
     style {
       background_color "#f5f5f5"
@@ -50,6 +52,30 @@ mu(:FILE_INDEX) {
         float 'left'
         padding '0 1em 0 0'
       }
+
+      h3 {
+        color          heading_color
+        text_transform 'uppercase'
+        font_size      'smaller'
+        padding        '0.5em 0'
+        margin         '0'
+      }
+
+      label {
+        display 'block'
+        span.^(:sub) {
+          color light_text_color
+        }
+      }
+
+      p {
+        margin '0 0 0.5em 0'
+      }
+
+      div.^(:footer) {
+        color light_text_color
+      }
+
     }
 
     title "megaUNI Homepage"
@@ -106,21 +132,14 @@ mu(:FILE_INDEX) {
           font_family 'AghjaMedium'
           font_weight 'normal'
           font_style  'normal'
-          color       '#357BB5'
+          color       heading_color
           margin      '0.5em 0'
 
           span.^(:main) { "mega" }
           span.^(:sub) {  "UNI" }
         }
 
-        div.id(:footer) {
-          style {
-            color '#B0B0B0'
-            p {
-              margin '0 0 0.5em 0'
-            }
-          }
-
+        div.id(:footer).^(:footer) {
           p  {
             raw_text "&copy;"
             text  " 2012-{{num.YEAR}} megauni.com. Some rights reserved."
