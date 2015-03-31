818e03c9559afc78eb82bf1a9d11c3ec1211821f
da99 <i-hate-spam-1234567@mailinator.com>
2015-03-31 16:30:31 -0500

    File re-org.

--- Server/Link/specs/02-read.screen_name.rb
+++ Server/Link/specs/02-read.screen_name.rb
@@ -1,8 +1,8 @@
 
-describe 'Link.read' do
+describe 'Link.read screen_name' do
 
   it "allows: STRANGER -> Screen_Name World Public" do
-    sn = Screen_Name.create screen_name: "sn_#{rand(1000)}"
+    sn = Screen_Name.create screen_name: "world_#{rand(1000)}"
     Screen_Name.update id: sn.data[:id], privacy: Screen_Name::WORLD
 
     link = Link.read(audience_id: nil, target_id: sn.id, type_id: Link::READ_SCREEN_NAME)
@@ -12,7 +12,7 @@ describe 'Link.read' do
 
   it "does not allow: Customer -> Screen_Name World Public, Blocked" do
     meanie = Screen_Name.create screen_name: "mean_#{rand(1000)}"
-    sn = Screen_Name.create screen_name: "sn_#{rand(10000)}"
+    sn = Screen_Name.create screen_name: "blocked_#{rand(10000)}"
     Screen_Name.update id: sn.data[:id], privacy: Screen_Name::WORLD
     Link.create owner_id: sn.data[:owner_id], type_id: Link::BLOCK, left_id: sn.id, right_id: meanie.id
 
@@ -25,49 +25,11 @@ describe 'Link.read' do
 
   it "allows: Customer -> Screen_Name Protected, Allowed" do
     friend = Screen_Name.create screen_name: "pheobe_#{rand(10000)}"
-    sn = Screen_Name.create screen_name: "sn_#{rand(1000)}"
+    sn = Screen_Name.create screen_name: "allow_#{rand(1000)}"
     Screen_Name.update id: sn.data[:id], privacy: Screen_Name::PROTECTED
     Link.create owner_id: sn.data[:owner_id], type_id: Link::ALLOW, left_id: sn.id, right_id: friend.id
 
     Link.read(audience_id: friend.data[:owner_id], target_id: sn.id, type_id: Link::READ_SCREEN_NAME).id.should == sn.id
   end # === it allows: Customer -> Screen_Name Protected, Allowed
 
-  it "allows: STRANGER -> POST from PUBLIC SCREEN_NAME" do
-    sn = Screen_Name.create(screen_name: "sn_#{rand(10000)}")
-    Screen_Name.update(id: sn.data[:id], privacy: 1)
-    computer = Computer.create(
-      owner_id: sn.data[:id],
-      code: {}
-    )
-
-    link = Link.read(
-      consumer_id: nil,
-      target_id:   computer.data[:id],
-      type_id:     :read_computer
-    )
-
-    link[:target_id].should == computer.data[:id]
-  end
-
-  it "disallows: STRANGER -> POST from PROTECTED SCREEN_NAME" do
-    sn = Screen_Name.create(screen_name: "sn_#{rand(10000)}")
-    Screen_Name.update(id: sn.data[:id], privacy: 2)
-    computer = Computer.create(
-      owner_id: sn.data[:id],
-      code: {}
-    )
-
-    link = Link.read(
-      consumer_id: nil,
-      target_id:   computer.data[:id],
-      type_id:     :read_computer
-    )
-
-    link.should = nil
-  end
-
-  it "allows: OWNER -> POST from PRIVATE SCREEN_NAME"
-
-  it "allows: CUSTOMER -> POST from PRIVATE SCREEN_NAME w/exceptions"
-
-end # === describe 'Link.read'
+end # === describe 'Link.read screen_name'
