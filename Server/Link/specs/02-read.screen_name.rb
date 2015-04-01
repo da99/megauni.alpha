74735245ae2005c0af01db62ce1c9b1054ae9726
da99 <i-hate-spam-1234567@mailinator.com>
2015-04-01 13:03:45 -0500

    Dev. checkpoint: Touchups.

--- Server/Link/specs/02-read.screen_name.rb
+++ Server/Link/specs/02-read.screen_name.rb
@@ -3,17 +3,17 @@ describe 'Link.read screen_name' do
 
   it "allows: STRANGER -> Screen_Name World Public" do
     sn = screen_name "world"
-    Screen_Name.update id: sn.data[:id], privacy: Screen_Name::WORLD
+    sn.is :WORLD
 
-    link = Link.read(audience_id: nil, target_id: sn.id, type_id: Link::READ_SCREEN_NAME)
+    link = Link.read(:SCREEN_NAME, nil, sn.id)
 
     link.id.should == sn.id
   end
 
   it "does not allow: Customer -> Screen_Name World Public, Blocked" do
-    meanie = screen_name "mean"
-    sn = Screen_Name.create screen_name: "blocked_#{rand(10000)}"
-    Screen_Name.update id: sn.id, privacy: Screen_Name::WORLD
+    meanie = screen_name "meanie"
+    sn     = screen_name "blocked"
+    sn.is :WORLD
     Link.create owner_id: sn.data[:owner_id], type_id: Link::BLOCK_ACCESS_SCREEN_NAME, left_id: meanie.id, right_id: sn.id
 
     read = {audience_id: meanie.data[:owner_id], target_id: sn.id, type_id: Link::READ_SCREEN_NAME}
@@ -26,7 +26,7 @@ describe 'Link.read screen_name' do
   it "allows: Customer -> Screen_Name PROTECTED, Allowed" do
     friend = screen_name "pheobe"
     sn     = screen_name "allow"
-    Screen_Name.update id: sn.id, privacy: Screen_Name::PROTECTED
+    sn.is :PROTECTED
     friend.is_allowed_to_read(sn)
 
     Link.read(Link::READ_SCREEN_NAME, friend.data[:owner_id], sn.id ).id.should == sn.id
@@ -36,7 +36,7 @@ describe 'Link.read screen_name' do
     sn = screen_name("pro")
     sn.is :protected
 
-    Link.read(:READ_SCREEN_NAME, sn.data[:owner_id], sn.id).id.should == sn.id
+    Link.read(:SCREEN_NAME, sn.data[:owner_id], sn.id).id.should == sn.id
 
   end # === it allows: OWNER -> Screen_Name PROTECTED
 
