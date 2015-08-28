9d99f02f4f8946317d87955084fe99f512c3c622
da99 <i-hate-spam-1234567@mailinator.com>
2015-08-28 17:21:10 -0500

    Improved: efficiency

--- Server/User/migrates/0001-User-table.sql
+++ Server/User/migrates/0001-User-table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "user" (
   id                   serial PRIMARY KEY,
   perm_level           smallint     NOT NULL DEFAULT 0,
-  pswd_hash            varchar(100) NOT NULL,
+  pswd_hash            bytea        NOT NULL,
   log_in_at            date         NOT NULL DEFAULT current_date,
   bad_log_in_count     smallint     NOT NULL DEFAULT 0,
   created_at           timestamp with time zone NOT NULL DEFAULT timezone('UTC'::text, now()),
