e6151057de6e12c3486cf20339e0b373949a8866
da99 <i-hate-spam-1234567@mailinator.com>
2015-10-30 10:08:21 -0500

    Fixed: Spec.ex was always being run by being in lib/

--- lib/Link/migrates/001-table.sql
+++ lib/Link/migrates/001-table.sql
@@ -1,16 +1,4 @@
 
--- LINK_TYPE_ID = 1
-  --  BLOCK_ACCESS_SCREEN_NAME         = 10 # meanie  -> me
-  --  BLOCK_ACCESS_TO_ALL_SCREEN_NAMES = 11 # meanie  -> me
-  --  ALLOW_ACCESS_SCREEN_NAME         = 12 # friend  -> me
-
-  --  link CARD to SN                  = 20     # sn_id, card.id -> sn.id
-  --  ALLOW_TO_LINK                    = 21     # friend  -> target
-  --  LAST READ AT                     = 22     # owner_id -> owner_id => sn.id
-  --  follow                           = 23     # sn, sn -> target
-  --  ALLOW TO READ CARD               = 24     # sn, sn -> card.id
-
-  --  COMMENT                          = 30     # comment -> post
 
 -- "I want to post content to my screen name:"
   --  owner_id -> a          ->  b
