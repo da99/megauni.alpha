8200d5afd8329cd6d18a1225d187f4efb783b072
da99 <i-hate-spam-1234567@mailinator.com>
2015-11-01 07:30:58 -0600

    Various bug fixes

--- lib/Card/migrates/__-01-can_read_card.sql
+++ lib/Card/migrates/__-01-can_read_card.sql
@@ -47,15 +47,15 @@ BEGIN
     card
   WHERE
     -- WORLD READable, bypassing SN permission
-    card.privacy = 4
+    card.privacy = name_to_type_id('WORLD READABLE')
 
     OR
     -- SAME AS SN:
-    ( card.privacy = 3 AND EXISTS (SELECT * FROM can_read(SN_ID, card.owner_id)) )
+    ( card.privacy = name_to_type_id('SAME AS OWNER') AND EXISTS (SELECT * FROM can_read(SN_ID, card.owner_id)) )
 
     OR
     -- AUD must be on list allowed card readers to read:
-    ( card.privacy = 2 AND EXISTS (SELECT * FROM in_card_read_list_of(SN_ID, card.id)) )
+    ( card.privacy = name_to_type_id('LIST ONLY') AND EXISTS (SELECT * FROM in_card_read_list_of(SN_ID, card.id)) )
   LIMIT 1
   ;
 
--- lib/Link/migrates/__-02-link_insert.sql
+++ lib/Link/migrates/__-02-link_insert.sql
@@ -10,24 +10,30 @@ CREATE OR REPLACE FUNCTION link_insert (
   IN TYPE_NAME    VARCHAR,
   IN A_ID         INT,
   IN SCREEN_NAME  VARCHAR
-) RETURNS SMALLINT[]
+) RETURNS TABLE(id INT)
 AS $$
-DECLARE
 BEGIN
   CASE TYPE_NAME
   WHEN 'LINK | CARD, SN', 'LINK | CARD, SCREEN_NAME' THEN
-    INSERT INTO link (owner_id, type_id, a_type_id, a_id, b_type_id, b_id)
+    RETURN QUERY
+    INSERT INTO link (
+      type_id,
+      owner_id, owner_type_id,
+      a_type_id, a_id,
+      b_type_id, b_id
+    )
     VALUES (
-      USER_ID,
+      name_to_type_id('LINK'),
 
-      name_to_link_type_id('LINK'),
+      name_to_type_id('SN'),
+      screen_name_id_of_or_fail(USER_ID, SCREEN_NAME),
 
       name_to_type_id('CARD'),
       return_card_id_or_fail(USER_ID, A_ID),
 
       name_to_type_id('SN'),
       screen_name_id_of_or_fail(USER_ID, SCREEN_NAME)
-    );
+    ) RETURNING link.id;
   ELSE
     RAISE EXCEPTION 'programmer_error: unknown link_insert type: %', TYPE_NAME;
   END CASE;
