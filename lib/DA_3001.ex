3a317a523ca90c93d81d5d7a5f458dc2e385e498
da99 <i-hate-spam-1234567@mailinator.com>
2015-11-10 04:40:57 -0600

    Added: DA_3001

---
+++ lib/DA_3001.ex
@@ -0,0 +1,33 @@
+
+defmodule DA_3001 do
+
+  def to_file_paths(string) when is_string(string) do
+    string |> Path.wildcard
+  end
+
+  def to_file_paths(list) when is_list(list) do
+    Enum.map(list, &(to_file_paths &1))
+    |> List.flatten
+  end
+
+  def split(list, delim) when is_list(list) do
+    Enum.map(list, &(String.split(&1, delim))
+  end
+
+  def at(list, pos) when is_list(list) do
+    list |> Enum.map(&(Enum.at &1, 1))
+  end
+
+  def pluck(list, pos) when is_list(list) and is_number(pos) do
+    list |> Enum.map(&(Enum.at &1, pos))
+  end
+
+  def pluck(map, key) when is_map(map) do
+    map |> Enum.map(&(Enum.at &1, key))
+  end
+
+  def map(list, func) when is_list(list) do
+    Enum.map list, func
+  end
+
+end # === defmodule DA_3001
--- spec/Spec.ex
+++ spec/Spec.ex
@@ -1,43 +1,51 @@
 
 
-models = if Enum.empty?(System.argv) do
-  {raw, _exit} = System.cmd Path.expand("bin/megauni"), ["model_list"]
-  raw |> String.strip |> String.split
-else
-  System.argv
-end
-
-files = Enum.reduce models, [], fn(mod, acc) ->
-   cond do
-     File.exists?(mod) ->
-       acc ++ [mod]
-     mod =~ ~r/[\*|\/]/ ->
-       acc ++ Path.wildcard("lib/#{mod}.json")
-     true ->
-       acc ++ Path.wildcard("lib/#{mod}/specs/*.json")
-   end
-end
 
 
-if Enum.empty?(files) do
-  Process.exit self, "!!! No specs found: #{inspect System.argv}. Exiting..."
-end
+defmodule Main do
 
-spec_funcs = "lib/*/specs/funcs.ex*"
-              |> Path.wildcard
-              |> Enum.map(&(String.split &1, "/"))
-              |> Enum.map(&(Enum.at &1, 1))
-              |> Enum.map(&(:"Elixir.#{x}.Spec_Funcs"))
+  import DA_3001, only: :functions
+
+  def modules do
+    "lib/*/specs/funcs.ex*"
+    |> to_files
+    |> split("/")
+    |> pluck(1)
+    |> map(&(:"Elixir.#{&1}.Spec_Funcs"))
+  end
+
+  def models do
+    models = if Enum.empty?(System.argv) do
+      {raw, _exit} = System.cmd Path.expand("bin/megauni"), ["model_list"]
+      raw |> String.strip |> String.split
+    else
+      System.argv
+    end
+  end # === def models
+
+  def files do
+    files = Enum.reduce models, [], fn(mod, acc) ->
+      cond do
+        File.exists?(mod) ->
+          acc ++ [mod]
+        mod =~ ~r/[\*|\/]/ ->
+          acc ++ Path.wildcard("lib/#{mod}.json")
+        true ->
+          acc ++ Path.wildcard("lib/#{mod}/specs/*.json")
+      end
+    end
+
+    if Enum.empty?(files) do
+      Process.exit self, "!!! No specs found: #{inspect System.argv}. Exiting..."
+    end
+    files
+  end # === def files
+
+end # === defmodule Main
 
 JSON_Applet.Spec.run_files(
-  files,
-  [
-    Screen_Name.Spec_Funcs,
-    User.Spec_Funcs,
-    Link.Spec_Funcs,
-    Log_In.Spec_Funcs,
-    Megauni.Spec_Funcs
-  ]
+  Main.files,
+  Main.modules
 )
 
 
