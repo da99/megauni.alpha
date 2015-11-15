fb034385bfa57d1b0b32337d7dd497eaccd3bbf9
da99 <i-hate-spam-1234567@mailinator.com>
2015-11-15 12:23:49 -0600

    Fixes and touchups

--- lib/Megauni/Megauni.ex
+++ lib/Megauni/Megauni.ex
@@ -20,3 +20,15 @@ defmodule Megauni do
   end
 
 end # === defmodule Megauni
+
+
+defmodule Mix.Tasks.Megauni do
+
+  defmodule Run do
+    use Mix.Task
+    def run(args) do
+      Mix.Task.run "run", args
+    end # === def run(_)
+  end # === defmodule Run
+
+end # === defmodule Mix.Tasks.Megauni
