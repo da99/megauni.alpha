1e7afe168dc82a6dfe324b8517e26826a8a4a295
da99 <i-hate-spam-1234567@mailinator.com>
2015-11-17 06:32:52 -0600

    Fixed: DRY up the version

--- mix.exs
+++ mix.exs
@@ -5,36 +5,37 @@ defmodule Megauni.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :megauni,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app:             :megauni,
+      version:         File.read!("VERSION") |> String.strip,
+      elixir:          "~> 1.0",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: [
+        {:cowboy   , "~> 1.0.0"},
+        {:plug     , "~> 1.0.0"},
+        {:postgrex , ">= 0.9.0"},
+        {:poison   , "~> 1.5.0"},
+        {:ecto     , "~> 1.0"},
+        {:comeonin , "> 1.1.0"},
+        {:timex    , "~> 0.19.5"}
+      ]
+    ]
   end
 
   def application do
     [
+      mod: {Megauni, []},
       applications: [
-        :logger, :postgrex,
-        :ecto, :cowboy,
-        :plug, :poison,
-        :comeonin, :tzdata
-      ],
-
-      mod: {Megauni, []}
-    ]
-  end
-
-  defp deps do
-    [
-      {:cowboy   , "~> 1.0.0"},
-      {:plug     , "~> 1.0.0"},
-      {:postgrex , ">= 0.9.0"},
-      {:poison   , "~> 1.5.0"},
-      {:ecto     , "~> 1.0"},
-      {:comeonin , "> 1.1.0"},
-      {:timex    , "~> 0.19.5"}
+        :cowboy,
+        :comeonin,
+        :ecto,
+        :logger,
+        :postgrex,
+        :plug,
+        :poison,
+        :tzdata
+      ]
     ]
   end
 
