18835bf7bf6a0967da55a8035c25712e4b32f836
da99 <i-hate-spam-1234567@mailinator.com>
2016-03-26 09:58:52 -0500

    Removed: obsolete files. Merged: redundant files

---
+++ Docs/Uni_App.rb
@@ -0,0 +1,186 @@
+
+Mini-Newspaper (for each life, each gets a custom debate page.)
+  - Posts
+    - view_id 
+      1 - Public
+      2 - Friends
+      3 - Friends & Fans
+      4 - Let me select audience:
+  - PostComments [ "Important News", "A Random Thought"]
+  - PostQuestions [ "Important Question", "A Silly Question" ]
+  - PostViewers
+  - club_id
+  |
+Network
+  - TightPersons
+  - LoosePersons
+  - TightPersonInvite
+  |
+Clubs
+  - TodoLists
+  - Predictions
+  - Questions
+  - News (Debates)
+  |
+TODOS
+  |
+Pets
+  |
+Questions 
+  |
+Translation
+  - to English
+  - to Japanese
+  |
+Lunch Dating
+  Find Breeder
+  Find Partner
+  Complain
+  Advice/Tips/Warnings
+  |
+Housing
+  Rent Out
+  Find
+  Mice
+  Cleaning
+  |
+University
+  Rate Professors
+  Post Warnings/News
+  Find/Create A College
+  |
+Blogs + Newspaper
+  |
+Travel & Dining
+  Find a city
+  Post a city
+  |
+News
+  |
+Layman Encyclopedia/Search (=Brain)
+(Unify Wikipedia-Clone with Google-clone + Bing clone)
+  |
+Corporal Captitalists (bonds in working individuals)
+
+
+
+
+
+# ------- TABLES ---------------------
+NewsComments
+  - news_id
+  - status = PENDING || ACCEPTED || REJECTED
+  - category = PRAISE || DENOUNCE || FACT CHECK || QUESTION || RANDOM
+  - parent_id # for answering questions posted in the comments.
+NewsCommentSections
+
+News
+  - parent_id # for news branching (predictions or responses).
+  - language_id
+  - category = DOINGS || NEWS || PREDICTIONS || OPINIONS || QUESTIONS
+NewsEdits  
+
+
+
+
+- version 1
+  - site permission levels
+    - admin
+    - editor/moderator
+    - unlimited invitations
+  - multiple identities
+  - pet profiles
+    - med condition
+  - baby profiles
+    - med condition
+  - pre-born profiles
+    - names
+  - fictional profiles
+  - photo management
+  - youtube linking
+  - photo linking
+  - guides/pamphlets
+  - people mananagement
+    - birthdays, anniversaries, important dates, repeating dates
+
+  - Q&A
+    - translations
+    - vote best answer
+    - competance weights
+    
+    
+  - daily and onetime checklists
+    - vitamins, etc.
+    - countdowns, but no sound  
+    - sharable
+    - rules-based  
+  - project management
+    - due dates
+      - status
+    - milestones
+    - files
+  - office management
+    - tweets
+      - labels   
+    - news
+    - calendar
+    - vote for best answer
+      - translation
+      - gardening
+      - engineering
+      - etc. 
+      
+  - invitations
+    - gender
+    - group
+    
+  - following
+    - friends
+    - fans
+    - family
+    - co-workers
+    - frienemies
+    - enemies
+    - ex-lovers
+    
+  - tweets with labels   
+    - No SMS for now.
+   
+    
+    
+- Future version   
+  - bug tracking
+  - visualize data stream (help handle data overload)
+    - inspiration: plurk
+  - email broadcasting
+    - newsletters paid for 250 or above
+  - video management
+  - community management 
+  - YouTube account connection  
+  - Market 
+    - local services
+      - cleaning
+      - food delivery  
+
+  - reputation
+    - import/export  
+   
+  - footprints
+    - request to see profile
+    - freind only profiles
+  
+  - universal language
+
+- Create stories for learning alphabet and kanji characters
+  - Video.
+  - Slides.
+- Vote on translations.
+- Vote on pronounciation. (MP3/OggVorbis)
+
+
+Future 
+- Job board.
+- Advice/Help section
+- News section
+- Video news w/translation.
+- Postcard to Bill Sardi.     
