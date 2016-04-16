4cd12df4e5090d6849668d7ac9c55fe32a3c04a6
da99 <i-hate-spam-1234567@mailinator.com>
2016-04-15 21:31:02 -0500

    Fixed: various

--- bin/lib/build.sh
+++ bin/lib/build.sh
@@ -1,9 +1,22 @@
 
 # === {{CMD}}
+# === {{CMD}}  "NAME"
 build () {
+  if [[ -z "$@" ]]; then
+    mksh_setup ORANGE "=== {{Building}}..." >&2
+    build "Homepage"
+    mksh_setup GREEN "=== Done {{building}}." >&2
+    return 0
+  fi
+
+  local +x NAME="$1"; shift
+  local +x INPUT="Server/$NAME"
+  local +x OUTPUT="Public/applets/$NAME"
+  mkdir -p "$OUTPUT"
+
   cd "$THIS_DIR"
   dum_dum_boom_boom html           \
-    --input-dir "Server/"           \
-    --output-dir "Public/applets"    \
+    --input-dir  "$INPUT"           \
+    --output-dir "$OUTPUT"           \
     --public-dir "Public/"
 } # === end function
