68cd6e8fd373a42a00b3ea17a882822017770e33
da99 <i-hate-spam-1234567@mailinator.com>
2016-05-10 15:48:04 -0500

    Ensure: commands are run only in DEV machine.

--- bin/lib/snapshot.sh
+++ bin/lib/snapshot.sh
@@ -4,26 +4,30 @@ snapshot () {
 
   local +x DIR="$THIS_DIR/config/mariadb_snapshot"
 
-  if [[ -z "$IS_DEV" ]]; then
-    mksh_setup RED "!!! Snapshot tried to be made in non-{{DEV}} machine."
-    exit 1
-  fi
+  if mksh_setup is-dev; then
 
-  if [[ -d "$DIR" ]]; then
-    local +x INVALID_FILES="$(find "$DIR" -type f | grep -v ".sql")"
-    if [[ ! -z "$INVALID_FILES" ]]; then
-      mksh_setup RED "!!! Invalid files:\n$INVALID_FILES"
-      exit 1
+    if [[ -d "$DIR" ]]; then
+      local +x INVALID_FILES="$(find "$DIR" -type f | grep -v ".sql")"
+      if [[ ! -z "$INVALID_FILES" ]]; then
+        mksh_setup RED "!!! Invalid files:\n$INVALID_FILES"
+        exit 1
+      fi
+
+      mksh_setup ORANGE "=== Removing: {{$DIR}}"
+      rm -rf "$DIR"
     fi
 
-    mksh_setup ORANGE "=== Removing: {{$DIR}}"
-    rm -rf "$DIR"
-  fi
+    mariadb_setup snapshot "$DIR"
 
-  mariadb_setup snapshot "$DIR"
+    local +x GRANTS="$DIR/megauni.localhost.grants.sql"
+    echo "$(echo "SHOW GRANTS FOR 'megauni'@'localhost' ;" | mysql | tail -n+3 | sed 's/DROP[\,\ ]\+//')" > "$GRANTS"
 
-  local +x GRANTS="$DIR/megauni.localhost.grants.sql"
-  echo "$(echo "SHOW GRANTS FOR 'megauni'@'localhost' ;" | mysql | tail -n+3 | sed 's/DROP[\,\ ]\+//')" > "$GRANTS"
+    return 0
+
+  fi # ==== if is-dev
+
+  ensure-no-drop-grants
+  ensure-dev-snapshot matches prod snapshot
 
 } # === end function
 
